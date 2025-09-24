b90baf6e88ff7a5027956df92b7505fe6c0c4211
tristanchenxm <caiweicaiwei@gmail.com>
2025-09-24 15:40:49 +0800

    support latest version of mybatis

--- src/main/java/nameless/common/mybatis/parser/XMLLanguageDriver.java
+++ src/main/java/nameless/common/mybatis/parser/XMLLanguageDriver.java
@@ -9,22 +9,24 @@ import org.apache.ibatis.session.Configuration;
 
 import java.lang.reflect.Field;
 
-import static nameless.common.mybatis.parser.IteratorTokenHandler.ITERATOR_CLOSE_TAG;
-import static nameless.common.mybatis.parser.IteratorTokenHandler.ITERATOR_OPEN_TAG;
+import static com.pintar.common.mybatis.parser.IteratorTokenHandler.ITERATOR_CLOSE_TAG;
+import static com.pintar.common.mybatis.parser.IteratorTokenHandler.ITERATOR_OPEN_TAG;
+
 
 public class XMLLanguageDriver extends org.apache.ibatis.scripting.xmltags.XMLLanguageDriver {
 
     @Override
     public SqlSource createSqlSource(Configuration configuration, XNode script, Class<?> parameterType) {
-        String sqlScript = script.getStringBody();
+        String sqlScriptBefore = script.getStringBody().trim();
         IteratorTokenHandler tokenHandler = new IteratorTokenHandler();
-        sqlScript = new GenericTokenParser(ITERATOR_OPEN_TAG, ITERATOR_CLOSE_TAG, tokenHandler).parse(sqlScript);
+        String sqlScriptAfter = new GenericTokenParser(ITERATOR_OPEN_TAG, ITERATOR_CLOSE_TAG, tokenHandler).parse(sqlScriptBefore);
         if (tokenHandler.isIteratorTagPresents()) {
             try {
                 Field bodyField = XNode.class.getDeclaredField("body");
                 bodyField.setAccessible(true);
-                bodyField.set(script, sqlScript);
-                XPathParser parser = new XPathParser(script.toString(), false, configuration.getVariables(), new XMLMapperEntityResolver());
+                bodyField.set(script, sqlScriptAfter);
+                String xml = getXml(script, sqlScriptBefore, sqlScriptAfter);
+                XPathParser parser = new XPathParser(xml, false, configuration.getVariables(), new XMLMapperEntityResolver());
                 return super.createSqlSource(configuration, parser.evalNode("/" + script.getName()), parameterType);
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -32,6 +34,22 @@ public class XMLLanguageDriver extends org.apache.ibatis.scripting.xmltags.XMLLa
         }
 
         return super.createSqlSource(configuration, script, parameterType);
+    }
+
+    /**
+     * Get the XML string of the XNode, and replace the old SQL script with the new one.
+     * @param script the XNode
+     * @param sqlScriptBefore the old SQL script
+     * @param sqlScriptAfter the new SQL script
+     * @return the new XML string
+     */
+    private static String getXml(XNode script, String sqlScriptBefore, String sqlScriptAfter) {
+        String xml = script.toString();
+        int indexOfOldSqlScript = xml.indexOf(sqlScriptBefore.trim());
+        if (indexOfOldSqlScript >= 0) {
+            xml = xml.substring(0, indexOfOldSqlScript) + sqlScriptAfter + xml.substring(indexOfOldSqlScript + sqlScriptBefore.length());
+        }
+        return xml;
     }
 
     @Override
